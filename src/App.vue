6baef8a87948e32897337102f107fc0ccf098530
ResumeKTech <nganhkhoa.becloud@gmail.com>
2025-06-24 11:36:19 +0700

    Example

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <script>
 export default {
-  setup () {
-    const message = "Hello"
-
+  data() {
     return {
-      message
+      count: 0
+    }
+  },
+  methods: {
+    increase() {
+      this.count++
     }
   }
 }
 </script>
 
 <template>
-  <h1>{{ message }}</h1>
+  <h1>{{ count }}</h1>
+  <button @click="increase">Tăng số</button>
 </template>
 
 <style scoped>
+button {
+  padding: 6px 12px;
+  font-size: 16px;
+}
 </style>
