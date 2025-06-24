9d033ad3498ebf801494dfb87ccf852e0352265c
ResumeKTech <nganhkhoa.becloud@gmail.com>
2025-06-24 11:01:51 +0700

    Computed Properties

--- src/components/TagsInput.vue
+++ src/components/TagsInput.vue
@@ -11,19 +11,29 @@
     @keydown.enter="addNewTag"
     @keydown.delete="removeLastTag"
     @keydown.tab.prevent="addNewTag"
-    :class="{ 'tag-exists': tags.includes(newTag) }"
+    :class="{ 'tag-exists': isTagExists }"
   />
 </template>
 
 <script>
 export default {
+  //Nơi lưu trữ trạng thái State
   data: () => ({
     tags: ["vue", "react", "angular"],
     newTag: "",
   }),
+
+  // Tự động cập mỗi khi tags hoặc newTag thay đổi
+  // Trả về true nếu newTag đã tồn tại trong tags <=> false
+  // Dùng để tránh thêm trùng tag và đổi giao diện input khi bị trùng
+  computed: {
+    isTagExists() {
+      return this.tags.includes(this.newTag);
+    },
+  },
   methods: {
     addNewTag() {
-      if (this.newTag) {
+      if (this.newTag && !this.isTagExists) {
         this.tags.push(this.newTag);
         this.newTag = "";
       }
