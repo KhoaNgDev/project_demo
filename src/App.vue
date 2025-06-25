05cefc5817c953a40434a5a4db0a333c4054089d
Nguyễn Khoa <134806583+ResumeKNTech@users.noreply.github.com>
2025-06-25 11:22:25 +0700

    B1:   Sử dụng hàm ref để xác định tham chiếu phản ứng

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <script>
+// Sử dụng hàm ref để xác định tham chiếu phản ứng
 import { ref } from "vue";
 export default {
   setup() {
-    const count = ref(0)
-    const increase = () => count.value++
+    const count = ref(0);
+    const increase = () => count.value++;
+    const decrement = () => count.value--;
 
-    return { count, increase }
-  }
-}
+    return { count, increase, decrement };
+  },
+};
 </script>
 
 <template>
   <h1>{{ count }}</h1>
   <button @click="increase">Tăng số</button>
+  <button @click="decrement">Giảm số</button>
 </template>
 
 <style scoped>
